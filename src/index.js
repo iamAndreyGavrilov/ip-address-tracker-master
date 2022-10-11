ea52bfb2ea73a8106c2f1a516255e46d8a62a054
Andrey G <a.gavrilov@promo-bot.ru>
2022-10-11 18:48:12 +0500

    полученные данные отрисовать в DOM в блок вывода

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import { validateIp } from "./helpers";
 const ipInput = document.querySelector(".search-bar__input");
 const btn = document.querySelector(".search-bar__btn");
 
+const ipInfo = document.querySelector("#ip");
+const locationInfo = document.querySelector("#location");
+const timeZoneInfo = document.querySelector("#timezone");
+const ispInfo = document.querySelector("#isp");
+
 btn.addEventListener("click", getData);
 ipInput.addEventListener("keydown", handleKey);
 
@@ -14,7 +19,7 @@ function getData() {
       `https://geo.ipify.org/api/v2/country?apiKey=at_ZwQDQmmmU8nujDzeu0Zj3ZLvWwWJY&ipAddress=${ipInput.value}`
     )
       .then((response) => response.json())
-      .then(console.log);
+      .then((data) => setInfo(data));
   }
 }
 
@@ -22,4 +27,11 @@ function handleKey(e) {
   if (e.key === "Enter") {
     getData();
   }
+}
+
+function setInfo(mapData) {
+  ipInfo.innerText = mapData.ip;
+  locationInfo.innerText = `${mapData.location.country} ${mapData.location.region}`;
+  timeZoneInfo.innerText = mapData.location.timezone;
+  ispInfo.innerText = mapData.isp;
 }
