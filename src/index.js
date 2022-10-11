6582ff3f8599cd81d85f097d901a3d9ce2a3a242
Andrey G <a.gavrilov@promo-bot.ru>
2022-10-11 17:57:44 +0500

    добавлен функционал получения данных по IP

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('hello');
+const ipInput = document.querySelector(".search-bar__input");
+const btn = document.querySelector(".search-bar__btn");
+
+btn.addEventListener("click", getData);
+ipInput.addEventListener("keydown", handleKey);
+
+function getData() {
+  // проверка данных
+
+  fetch(
+    `https://geo.ipify.org/api/v2/country?apiKey=at_ZwQDQmmmU8nujDzeu0Zj3ZLvWwWJY&ipAddress=${ipInput.value}`
+  )
+    .then((response) => response.json())
+    .then(console.log);
+}
+
+function handleKey(e) {
+  if (e.key === "Enter") {
+    getData();
+  }
+}
